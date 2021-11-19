395e89ca6f9a6ccb10a61f87ea27654fe31bcb29
Cesar Gonzalez <cegonzalez@expediagroup.com>
2021-11-18 16:51:54 -0800

    ADD TailwindCSS

--- src/Pet.js
+++ src/Pet.js
@@ -1,26 +1,18 @@
 import { Link } from "react-router-dom";
 
-const Pet = ({
-  name,
-  animal,
-  breed,
-  images,
-  location,
-  id
-}) => {
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "https://pets-images.dev-apis.com/pets/none.jpg";
 
-  let hero = 'https://pets-images.dev-apis.com/pets/none.jpg';
-
-  if(images.length){
+  if (images.length) {
     hero = images[0];
   }
 
   return (
-    <Link to={`/details/${id}`} className="pet">
-      <div className="image-container">
+    <Link to={`/details/${id}`} className="relative block">
+      <div>
         <img src={hero} alt={name} />
       </div>
-      <div className="info">
+      <div className="absolute bottom-0 left-0 bg-gradient-to-tr from-white to-transparent pr-2 pt-2">
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
